9b8a3d426075ace08e4469bb25d98112a186f941
TelegramCoder <107472681+TelegramCoder@users.noreply.github.com>
2025-02-27 17:40:59 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "avrill.v", quotes: "Setiap kata dan tindakan yang bikin km kecewa it bukanlah maksudku. Aku sangat menyesal dan mohon maaf. Aku janji akan berusaha lebih baik, maafin ya cintaku gresss." },
+    { author: "antjedtta_", quotes: "Setiap kata dan tindakan yang bikin km kecewa it bukanlah maksudku. Aku sangat menyesal dan mohon maaf. Aku janji akan berusaha lebih baik, maafin ya cintaku gresss." },
+    { author: "standinshd", quotes: "Setiap kata dan tindakan yang bikin km kecewa it bukanlah maksudku. Aku sangat menyesal dan mohon maaf. Aku janji akan berusaha lebih baik, maafin ya cintaku gresss." },
+    { author: "standinshd", quotes: "Setiap kata dan tindakan yang bikin km kecewa it bukanlah maksudku. Aku sangat menyesal dan mohon maaf. Aku janji akan berusaha lebih baik, maafin ya cintaku gresss." },
+    { author: "standinshd", quotes: "Setiap kata dan tindakan yang bikin km kecewa it bukanlah maksudku. Aku sangat menyesal dan mohon maaf. Aku janji akan berusaha lebih baik, maafin ya cintaku gresss. },
+    { author: "standinshd", quotes: "Setiap kata dan tindakan yang bikin km kecewa it bukanlah maksudku. Aku sangat menyesal dan mohon maaf. Aku janji akan berusaha lebih baik, maafin ya cintaku gresss. },
+    { author: "standinshd", quotes: "Setiap kata dan tindakan yang bikin km kecewa it bukanlah maksudku. Aku sangat menyesal dan mohon maaf. Aku janji akan berusaha lebih baik, maafin ya cintaku gresss.I'm sorry for loving you." },
 ];
